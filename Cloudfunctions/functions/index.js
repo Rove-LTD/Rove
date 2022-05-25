c2b48fe7b1ae3aedf0d18d82b4e764dd774f518d
buckrogers99 <williamrgrs74@gmail.com>
2022-05-25 13:08:42 +0100

    Added strava deauthorisation function
    Still need to add a function to send a response to the dev

--- Cloudfunctions/functions/index.js
+++ Cloudfunctions/functions/index.js
@@ -1,9 +1,39 @@
 const functions = require("firebase-functions");
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
+const Url = require("url");
+const request = require("request");
 admin.initializeApp();
+const db = admin.firestore();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.stravaDeauth = functions.https.onRequest(async (req, res) => {
+  // requires userId & devId
+  const reqData = Url.parse(req.url, true).query;
+  const userId = reqData["userId"];
+  // const devId = req["devId"];
+  console.log(userId);
+  const userRef = db.collection("users").doc(userId);
+  const userDoc = await userRef.get();
+  const userAccessToken = userDoc["strava_access_token"];
+  const dataString = "access_token=" + userAccessToken;
+  const options = {
+    url: "https://www.strava.com/oauth/deauthorize",
+    method: "POST",
+    body: dataString,
+  };
+  request(options, async (error, response, body) => {
+    if (!error && response.statusCode) {
+      await userRef.set({"strava_connected": false});
+      // await sendDevSuccess(devId);
+    } else {
+      console.log(JSON.parse(body));
+      // send an error response to dev
+      // TODO create dev fail post
+    }
+  });
 });
