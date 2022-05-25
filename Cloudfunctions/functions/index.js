a1a720b8d066beef358b389b4aca4d8f3124eb0b
AlessandroVentisei <alessandro.ventisei@outlook.com>
2022-05-25 14:20:44 +0100

    WEBHOOKS WORK!!!

--- Cloudfunctions/functions/index.js
+++ Cloudfunctions/functions/index.js
@@ -104,3 +104,30 @@ function stravaOauth(req) {
   const baseUrl = "https://www.strava.com/oauth/authorize?";
   return (baseUrl + encodedParameters);
 }
+
+
+exports.stravaWebhook = functions.https.onRequest((request, response) => {
+  if (request.method === "POST") {
+    functions.logger.info("webhook event received!", {
+      query: request.query,
+      body: request.body,
+    });
+    response.status(200).send("EVENT_RECEIVED");
+  } else if (request.method === "GET") {
+    const VERIFY_TOKEN = "STRAVA";
+    const mode = request.query["hub.mode"];
+    const token = request.query["hub.verify_token"];
+    const challenge = request.query["hub.challenge"];
+
+    if (mode && token) {
+      if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        functions.logger.info("WEBHOOK_VERIFIED");
+        response.status(200).json({"hub.challenge": challenge});
+      } else {
+        response.sendStatus(403);
+      }
+    } else {
+      response.sendStatus(403);
+    }
+  }
+});
