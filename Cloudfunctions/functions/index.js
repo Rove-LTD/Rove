085e59222cb1655e55e6d28204b4f2387c05795b
AlessandroVentisei <alessandro.ventisei@outlook.com>
2022-06-16 09:15:58 +0100

    added status 400 to responses from connectService

--- Cloudfunctions/functions/index.js
+++ Cloudfunctions/functions/index.js
@@ -56,17 +56,20 @@ exports.connectService = functions.https.onRequest(async (req, res) => {
     if (!devDoc.exists) {
       url =
         "error: the developerId was badly formatted, missing or not authorised";
+      res.statusCode(400);
       res.send(url);
       return;
     }
     if (devDoc.data().devKey != devKey|| devKey == null) {
       url =
         "error: the developerId was badly formatted, missing or not authorised";
+      res.statusCode(400);
       res.send(url);
       return;
     }
   } else {
     url = "error: the developerId parameter is missing";
+    res.statusCode(400);
     res.send(url);
     return;
   }
@@ -74,6 +77,7 @@ exports.connectService = functions.https.onRequest(async (req, res) => {
   // now check the userId has been given
   if (userId == null) {
     url = "error: the userId parameter is missing";
+    res.statusCode(400);
     res.send(url);
     return;
   }
