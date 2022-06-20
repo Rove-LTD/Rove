68b5c368787065dd3e4cd68407360b9868efb2e3
Ben Thompson-Watson <benthompsonwatson@hotmail.co.uk>
2022-06-20 10:51:56 +0100

    small descriptive update all changes are online

--- Cloudfunctions/functions/oauthGoogle.js
+++ Cloudfunctions/functions/oauthGoogle.js
@@ -2,4 +2,7 @@
 // by Ben Thompson-Watson
 // 14/05/2022
 
-console.log("OK GOOGLE...")
+console.log("OK GOOGLE...");
+
+// Notes on progress. Credentials are setup online, just need to be linked to
+// a cloud function in the API.
