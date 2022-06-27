fc6ba55e9e6ce667eb6eb5c5ea83804ea79c61fc
ventisep <paul.ventisei@gmail.com>
2022-06-27 15:54:49 +0100

    Split Tests into Separate Files
    This change will make it easier to manage the tests by splitting the tests into separate files and putting parameters into one file that enables us to more easily change the parameters that the tests are using.
    
    I have also created a template file to more easily create new test files.

---
+++ Cloudfunctions/functions/.test/test.js
@@ -0,0 +1,59 @@
+// Follow the instructions in README.md for running these tests.
+// Visit https://firebase.google.com/docs/functions/unit-testing to learn more
+// about using the `firebase-functions-test` SDK.
+
+// -----------------------COMMON TEST SETUP---------------------------//
+// Chai is a commonly used library for creating unit test suites. It is easily 
+// extended with plugins.
+const chai = require('chai');
+const assert = chai.assert;
+// Sinon is a library used for mocking or verifying function calls in JavaScript.
+const sinon = require('sinon');
+// -------------------END COMMON TEST SETUP---------------------------//
+
+// -----------INITIALISE THE ROVE TEST PARAMETERS----------------------------//
+const testParameters = require('./testParameters.json');
+const firebaseConfig = testParameters.firebaseConfig;
+const testUser = testParameters.testUser
+const testDev = testParameters.testDev
+const devTestData = testParameters.devTestData
+const devUserData = testParameters.devUserData
+const test = require('firebase-functions-test')(firebaseConfig, testParameters.testKeyFile);
+const admin = require("firebase-admin");
+// -----------END INITIALISE ROVE TEST PARAMETERS----------------------------//
+
+describe('ROVE full integration test scripts', () => {
+
+    before(async() => {
+        //set up the database
+        //insert the developer ID and data
+        //this clears down any existing developer data
+        await admin.firestore()
+            .collection("developers")
+            .doc(testDev)
+            .set(devTestData);
+        
+        await admin.firestore()
+            .collection("users")
+            .doc(testUser)
+            .set(devUserData);
+                
+    }); //end before
+
+    after(async () => {
+        // Do cleanup tasks and
+        // restore any stubbed functions
+        sinon.restore();
+        test.cleanup();
+    }); //end after
+
+    require ('./test-modules/connectService.test.js');
+    require ('./test-modules/stravaCallback.test.js');
+    require ('./test-modules/polarCallback.test.js');
+    require ('./test-modules/wahooCallback.test.js');
+    require ('./test-modules/garminCallback.test.js');
+    require ('./test-modules/webhooks.test.js');
+    require ('./test-modules/utility.test.js');
+});
+// require ('./test-modules/index.test.js');
+// require ('./test-modules/utility.test.js');
